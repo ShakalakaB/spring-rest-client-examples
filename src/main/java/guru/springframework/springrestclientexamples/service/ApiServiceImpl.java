55bfa899999f058c90c138d9ecf322fe7f85a7b3
ShakalakaB <yunjie989@gmail.com>
2021-05-31 18:48:19 +0800

    Add url builde

--- src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -2,26 +2,34 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
 public class ApiServiceImpl implements ApiService {
     private final RestTemplate restTemplate;
+    private final String apiUrl;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${apiUrl}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(apiUrl)
+                .queryParam("_limit", limit);
+
         ResponseEntity<List<User>> responseEntity = restTemplate.exchange(
-                "https://jsonplaceholder.typicode.com/users?_limit=" + limit,
+                uriComponentsBuilder.toUriString(),
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<User>>() {});
